e4c5cb6b7cab8abf7fd5f0e1da3a3f68fc7e8d5b
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-21 18:39:08 -0400

    Create Component -Import CSS -Create Function for Covid Data

---
+++ src/components/CovidData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+function CovidData() {
+
+return (
+    <div class="data">
+    <h2 class="covid">Covid Data Here</h2>
+    </div>    
+)
+}
+
+export default CovidData;
