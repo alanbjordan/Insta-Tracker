5d98dbbef3354e96d393b19c2598884e64b275c9
MichaelBamba <mjbamba2@gmail.com>
2020-04-22 09:16:55 -0400

    hand pulled down info

--- src/components/CovidData.js
+++ src/components/CovidData.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "../App.css";
 
+let fakeData = {
+  id: "A thing",
+  name: "covid",
+  thing: "otherThing",
+};
+
 function CovidData() {
   return (
-    <div class="data">
+    <div class="datacomp">
       <h2 class="covid">Covid Data Here</h2>
+      <div class="data">
+        {" "}
+        <fakeData />
+      </div>
     </div>
   );
 }
