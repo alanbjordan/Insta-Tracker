be3f8b4b3785727add1362e7fadde9f2598ee2d8
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-22 08:47:05 -0400

    Create component -Import CSS -Create function for News

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+function News() {
+
+    return (
+        <div className='news'>
+            <h5 className='newsPlaceholder'>News Here</h5>
+        </div>
+    )
+}
+
+export default News;
