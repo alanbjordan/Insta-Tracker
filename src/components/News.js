1698532d7b40bb59183567ef9d4d16173bcc582e
jamariod <j25ghs99@aol.com>
2020-04-22 11:30:15 -0400

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 
-function News() {
+class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      articles: [],
+    };
+  }
 
-    return (
-        <div className='news'>
-            <h5 className='newsPlaceholder'>News Here</h5>
-        </div>
+  componentDidMount() {
+    fetch(
+      'http://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=3e5a4b43acad4e7485d9486c10fd4fc4'
     )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          articles: data.articles,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className='news'>
+        <div>
+          {this.state.articles.map((item, index) => {
+            return (
+              <div className='container' key={index}>
+                <h5 className='text'>{item.title}</h5>
+
+                <a href={item.url} target='_blank'>
+                  Read More
+                </a>
+                <p className='text'>{item.content}</p>
+              </div>
+            );
+          })}
+        </div>
+        ;
+      </div>
+    );
+  }
 }
 
 export default News;
