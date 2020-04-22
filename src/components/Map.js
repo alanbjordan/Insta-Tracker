41a2c340dfb02ec5e1cc72df5d2de633f218846b
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-22 08:46:15 -0400

    Refactoring -Update class name syntax

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,8 +5,8 @@ import '../App.css';
 function Map() {
 
 return (
-    <div class="map">
-        <h1 class="hello">Map Goes Here</h1>
+    <div className="map">
+        <h1 className="hello">Map Goes Here</h1>
     </div>    
 )
 
