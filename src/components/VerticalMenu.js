d0728fd67c6d5c6045f5c857b0560fdad3e82598
MichaelBamba <mjbamba2@gmail.com>
2020-04-23 07:24:22 -0400

    Minor refactor not linked to map was unclear what was wanted to be refactored

--- src/components/VerticalMenu.js
+++ src/components/VerticalMenu.js
@@ -2,26 +2,52 @@ import React from "react";
 import "../App.css";
 
 function VerticalMenu() {
-
-    return (
-        <div className="ui vertical menu" id="vertmenu">
-
-        <div className="item">
-          <div className="header">COVID-19 Data</div>
-          <div className="menu">
-            <button className="item" className="dataButton">Global</button>
-            <button className="item" className="dataButton">Country</button>
-            <button className="item" className="dataButton">State</button>
-          </div>
+  return (
+    <div className="ui vertical menu" id="vertmenu">
+      <div className="item">
+        <div className="header">COVID-19 Data</div>
+        <div className="menu">
+          <button className="item" className="dataButton">
+            Global
+          </button>
+          <button className="item" className="dataButton">
+            Country
+          </button>
+          <button className="item" className="dataButton">
+            State
+          </button>
         </div>
-        <div className="ui checkbox">
-          <input type="checkbox" name="example" />
-          <label>Churches</label>
-        </div>
-        <div className="ui checkbox">
-          <input type="checkbox" name="example" />
-          <label>Private</label>
-        </div>
+      </div>
+      <div className="section head"> Food Banks </div>
+      <div className="ui checkbox">
+        <input type="checkbox" name="example" />
+        <label>Churches</label>
+      </div>
+      <div className="ui checkbox">
+        <input type="checkbox" name="example" />
+        <label>Private</label>
+      </div>
+      <div className="section head"> Grocery </div>
+      <div className="ui checkbox">
+        <input type="checkbox" name="example" />
+        <label>Grocery</label>
+      </div>
+      <div className="section head"> Medical</div>
+      <div className="ui checkbox">
+        <input type="checkbox" name="example" />
+        <label>Pharmacy </label>
+      </div>
+      <div className="ui checkbox">
+        <input type="checkbox" name="example" />
+        <label>Quick clinic </label>
+      </div>
+      <div className="ui checkbox">
+        <input type="checkbox" name="example" />
+        <label>Hospital </label>
+      </div>
+      <div className="ui checkbox">
+        <input type="checkbox" name="example" />
+        <label>Testing center</label>
       </div>
     </div>
   );
