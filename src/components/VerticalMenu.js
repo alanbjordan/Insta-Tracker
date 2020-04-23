d4c0159ec6cc2e2e19c7c7bdae89c4cd665213ca
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-23 15:30:26 -0400

    Refactor -Remove Menu Buttons

--- src/components/VerticalMenu.js
+++ src/components/VerticalMenu.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import '../App.css';
+import './Component.css';
 
 function VerticalMenu() {
 
     return (
         <div className="ui vertical menu" id="vertmenu">
-
-        <div className="item">
-          <div className="header">COVID-19 Data</div>
-          <div className="menu">
-            <button className="item" className="dataButton">Global</button>
-            <button className="item" className="dataButton">Country</button>
-            <button className="item" className="dataButton">State</button>
-          </div>
-        </div>
         <div className="item">
             <div className="header">Testing Centers</div>
             <div className="ui checkbox">
