0c4709980ebdfaac5d24f94b8e56bb9bf000f235
Maalik McBurrows <mmcburrows1@gmail.com>
2020-04-23 22:08:40 -0400

    added a dropdown to select equipment, now to add event handlers and combine muscle group and equipment to retrieve exercise

--- src/components/exercise.jsx
+++ src/components/exercise.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 
 class Exercise extends Component {
     state = {
-        muscles: []
+        muscles: [],
+        equipment: [],
+        exercises: []
     }
 
     loadMuscle = async () => {
@@ -11,17 +13,27 @@ class Exercise extends Component {
         const data = response.results;
         return data; 
     }
+    
+    loadEquipment = async () => {
+        const url = await fetch('https://wger.de/api/v2/equipment/?format=json');
+        const response = await url.json();
+        const data = response.results;
+        return data; 
+    }
 
     componentDidMount = async () => {
         const muscleData = await this.loadMuscle();
+        const equipmentData = await this.loadEquipment();
         this.setState({ 
-           muscles : muscleData 
+           muscles : muscleData,
+           equipment: equipmentData
         })
         console.log('Muscles are: ', this.state.muscles)
+        console.log('Equipment is: ', this.state.equipment)
     }
 
     render() {
-        const { muscles } = this.state
+        const { muscles, equipment } = this.state
         return (
             <div>
                 What body part would you like to work?
@@ -36,9 +48,24 @@ class Exercise extends Component {
                         </label>
                     </form>
                 </div>
+                <br></br>
+                What equipment do you have available?
+                <div>
+                    <form>
+                        <label id="equipmentSelectLabel">SELECT A PIECE OF EQUIPMENT:
+                            <select id="equipmentChangeForm">
+                                {equipment.map(stuff => (
+                                    <option key={stuff.id} value={stuff.name}>{stuff.name}</option>
+                                ))}
+                            </select>
+                        </label>
+                    </form>
+                </div>
             </div>
         )
     }
 }
 
 export default Exercise;
+
+// Link to get exercises based on muscle group and equipment available https://wger.de/api/v2/exercise/?language=2&muscles={this.state.muscles}&equipment={this.state.equipment}
