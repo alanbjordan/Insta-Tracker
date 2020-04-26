50f01dd0751a9ad594f72356f4c5a36bd531f187
jamariod <j25ghs99@aol.com>
2020-04-26 13:45:31 -0400

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,7 +10,15 @@ return (
     <div>
 
     
-   <iframe src="https://studio.here.com/viewer/?project_id=e8bdb0c7-f853-4840-968d-d4f57547c09a" frameborder="0"></iframe>
+     <Iframe
+        width='100%'
+        height='100%'
+        frameborder='0'
+        style='border:0'
+        src='https://studio.here.com/viewer/?project_id=e8bdb0c7-f853-4840-968d-d4f57547c09a'
+        owfullscreen
+        frameborder='0'
+      ></Iframe>
     </div>  
 )
 
