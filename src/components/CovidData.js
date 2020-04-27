61b754ebe3b54816f25d9e5cb833b68d50e35ba3
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-27 02:11:45 -0400

    Refactor/Enhance -Add data end points to select button -Added handler functions to update state when a state is selected

--- src/components/CovidData.js
+++ src/components/CovidData.js
@@ -4,9 +4,12 @@ import './Component.css';
 
 class CovidData extends Component {
     state = {
+        update: false,
         options: [],
         data: [],
-        stateData: []
+        stateData: [],
+        countryData: [],
+        theState: ["AK"],
     }
 
     loadData = async (url) => {
@@ -25,12 +28,10 @@ class CovidData extends Component {
         const url = `https://covidtracking.com/api/v1/us/current.json`
         const countryData = await this.loadData(url);
         console.log("countryData =>", countryData[0]);
-        
         return this.setState({
             data: countryData[0]
         })
     }
-
     
     handleCountryChange = () => {
         this.setState({
@@ -38,12 +39,21 @@ class CovidData extends Component {
         })
     }
 
-   //StateData
-
-    handleStateChange = () => {
-        this.setState({
-            data: this.getStateData()
+    //StateData
+    getStateData = async () => {
+        const url = `https://covidtracking.com/api/v1/states/${this.state.theState}/daily.json`
+        const stateData = await this.loadData(url);
+        return this.setState({
+            data: stateData[0]
         })
+    }
+
+   //StateData
+    handleStateChange = async (event) => {
+        this.setState({
+            update: true,
+            data: this.getStateData()
+        }, this.handleChange(event));
     }
 
     async componentDidMount() {
@@ -52,34 +62,33 @@ class CovidData extends Component {
         this.setState({
             stateData
         })
-
+        this.getCountryData();
     }
 
-    handleSubmit = async () => {
-        
+    handleChange = async (event) => {        
+        this.setState({
+            theState: event.target.value,
+        }, this.getStateData);
     }
-
-
 
     render() {
-        const { data, stateData } = this.state;
-        return (
+        const { data, stateData} = this.state;
 
+        return (
             <div className="data">
-                <button>Global</button>
-                <button onClick={this.handleCountryChange}>Country</button>
-                <button>State</button>
-                <form>
-                    <label>State:<select>
-                        {stateData.map((state) => <option value={state.positive}>{state.state}</option>)}
+                <button onClick={this.handleCountryChange} id='usButton'>See U.S. Totals</button>
+                <h3>or</h3>
+                <h2>Select a State</h2>
+                <form  value={this.state.theState} onChange={this.handleChange}>
+                    <label>State:
+                    <select>
+                        {stateData.map((state) => <option key={state.state} value={state.state}>{state.state}</option>)}
                     </select> 
-                    </label>  
-                </form>
-                <h5>Last Update: {data.lastModified}</h5>
+                    </label> 
+                </form  >
                 <h5>Positive Test: {data.positive}</h5>
-                <h5>Recovered: {data.recovered}</h5>
-                <h5>Hospitalized Currently:  {data.hospitalizedCurrently}</h5>
-                <h5>Total Cases: {data.cases}</h5>
+                <h5>Total Deaths: {data.death}</h5>
+                <h5>Total Cases: {data.total}</h5>
             </div>
         )
     }
