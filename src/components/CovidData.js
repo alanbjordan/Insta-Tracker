72ab186ac6a6637cdabd44c9ab028b36c452c84a
Alan Jordan <alan.jordanbiz@gmail.com>
2020-04-28 23:12:31 -0400

    Refactor -Change key name

--- src/components/CovidData.js
+++ src/components/CovidData.js
@@ -27,14 +27,15 @@ class CovidData extends Component {
     getCountryData = async () => {
         const url = `https://covidtracking.com/api/v1/us/current.json`
         const countryData = await this.loadData(url);;
+        console.log('the country data => ',countryData[0])
         return this.setState({
-            data: countryData[0]
+            countryData: countryData[0]
         })
     }
     
     handleCountryChange = () => {
         this.setState({
-            data: this.getCountryData()
+            countryData: this.getCountryData()
         })
     }
 
@@ -43,7 +44,7 @@ class CovidData extends Component {
         const url = `https://covidtracking.com/api/v1/states/${this.state.theState}/daily.json`
         const stateData = await this.loadData(url);
         return this.setState({
-            data: stateData[0]
+            data: stateData[0],
         })
     }
 
@@ -72,27 +73,39 @@ class CovidData extends Component {
     }
 
     render() {
-        const { data, stateData} = this.state;
+        const {stateData} = this.state;
+        const { data } = this.state;
         const theState = this.state.theState;
         const lcState = theState.toLowerCase();
+        const countryData = this.state.countryData;
         return (
             <div className="data">
-                <button onClick={this.getStateFlag}>Get State Flag</button>
-                <button onClick={this.handleCountryChange} id='usButton'>See U.S. Totals</button>
-                <h3>or</h3>
+                
+                <div className='resultsDiv'>
+                    <img src='https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png' className='flag' alt='state flag'/>
+                    <div>
+                        <h3 className='results1'>Positive Test: </h3><h5>{countryData.positive}</h5>
+                        <h3 className='results1'>Total Deaths: </h3><h5>{countryData.death}</h5>
+                        <h3 className='results1'>Total Cases: </h3><h5>{countryData.total}  </h5>                     
+                    </div>
+                </div>
                 <h2>Select a State</h2>
                 <form  value={this.state.theState} onChange={this.handleChange}>
                     <label>State:
-                    <select>
-                        {stateData.map((state) => <option key={state.state} value={state.state}>{state.state}</option>)}
+                    <select class="ui selection dropdown">
+                        {stateData.map((state) => <option  class="dropdown icon" key={state.state} value={state.state}>{state.state}</option>)}
                     </select> 
                     </label> 
                 </form  >
-                <hr />
-                <img src={`http://flags.ox3.in/svg/us/${lcState}.svg`} className='flag' alt='state flag'/>
-                <h3>Positive Test: </h3>{data.positive}
-                <h5>Total Deaths: </h5>{data.death}
-                <h5>Total Cases: </h5>{data.total}
+                <div className='resultsDiv'>
+                    <img src={`http://flags.ox3.in/svg/us/${lcState}.svg`} className='flag' alt='state flag'/>
+                    <div>
+                        <h3 className='results1'>Positive Test: </h3><h5>{data.positive}</h5>
+                        <h3 className='results1'>Total Deaths: </h3><h5>{data.death}</h5>
+                        <h3 className='results1'>Total Cases: </h3><h5>{data.total}  </h5>                     
+                    </div>
+                </div>
+
                 <hr />
             </div>
         )
