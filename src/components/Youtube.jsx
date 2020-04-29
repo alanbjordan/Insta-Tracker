290e60a203ba800826e90593848c44383962123d
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-28 22:05:45 -0400

    Refactor -remove comments -remove buttons -make component active on load

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -3,10 +3,7 @@ import React, { Component } from 'react';
 const API = 'AIzaSyCF6-Gp5GftHubZ6danNcSYQATv47ZtDS0'
 const playlistId = 'PLvrp9iOILTQaJa78zFQ0QgvShQ2HEwHxP'
 const maxResults = 10;
-const whoChannelId = 'UC07-dOwgza1IguKA86jqxNA'
-
 const cdcURL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=${maxResults}&playlistId=${playlistId}&key=${API}`
-const whoURL = `https://www.googleapis.com/youtube/v3/search?key=${API}&channelId=${whoChannelId}&part=snippet,id&order=date&maxResults=${maxResults}`
 
 class Youtube extends Component {
 
@@ -21,52 +18,23 @@ class Youtube extends Component {
         this.setState({
             ytResults
         })
-        console.log(this.state.ytResults);
         return ytResults;
     }
 
-    // whoClick = async() => {
-    //     const response = await fetch(whoURL);
-    //     const data = await response.json();
-    //     const ytResults = data.items.map(vid => 'https://www.youtube.com/embed/' + vid.id.videoId)
-    //     this.setState({
-    //         ytResults
-    //     })
-    //     console.log(this.state.ytResults);
-    //     return ytResults;
-    // }
-
     render() {
-        // console.log(URL);
         const { ytResults } = this.state
         return (
-            <div>
-                    <div>
-                        {/* <button onClick={this.cdcClick} type="button" class="btn btn-outline-danger">
-                            CDC Videos
-                        </button>
-                        <button onClick={this.whoClick} id="whoButton" class="btn btn-outline-danger">
-                            WHO Videos
-                        </button> */}
-                    </div>   
+            <div>   
                 <div className="youtube">
-  
-                    {
-                        ytResults.map((link, i) => {
+                    {ytResults.map((link, i) => {
                             var frame = <div key={i}><iframe title= "iframe" width="360" height="315" src={link} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe><br></br><br></br></div>
                             return frame;
-                        })
-                    }
-                    {this.frame}
-                                    
+                        })}
+                    {this.frame}               
                 </div>
             </div>              
         )
-
-
-      
     }
 }
-
 
 export default Youtube;
