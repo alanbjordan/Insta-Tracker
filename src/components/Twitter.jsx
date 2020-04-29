6b23e1c2d8b815ccd463b805e89b03df08c69805
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-28 22:05:14 -0400

    Refactor -Remove card styling

--- src/components/Twitter.jsx
+++ src/components/Twitter.jsx
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 class Twitter extends Component {
 
     render() {
         return (
-
-                        <TwitterTimelineEmbed
-                            sourceType="profile"
-                            screenName='cdcgov'
-                            options={{height: 360}}
-                        />
-
+            <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName='cdcgov'
+                options={{height: 360}}
+            />
         )
     }
 }
