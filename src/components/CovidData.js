e63248f07bad4d5498474e5052efacb17752304b
alanbjordan <alan.jordanbiz@gmail.com>
2020-04-29 21:52:39 -0400

    Refactor -Add chart to data component

--- src/components/CovidData.js
+++ src/components/CovidData.js
@@ -7,7 +7,7 @@ class CovidData extends Component {
     state = {
         update: false,
         options: [],
-        data: [],
+        data:[],
         stateData: [],
         countryData: [],
         theState: "ak",
@@ -28,7 +28,6 @@ class CovidData extends Component {
     getCountryData = async () => {
         const url = `https://covidtracking.com/api/v1/us/daily.json`
         const countryData = await this.loadData(url);;
-        console.log('the country data => ',countryData[0])
         return this.setState({
             countryData: countryData[0]
         })
@@ -37,9 +36,16 @@ class CovidData extends Component {
     getChartData = async () => {
         const url = `https://covidtracking.com/api/v1/us/daily.json`
         const chartData = await this.loadData(url);
-        console.log('chart data => ', chartData);
         return this.setState({
             chartData: chartData
+        })
+    }
+
+    getStateHistory = async () => {
+        const url = `https://covidtracking.com/api/v1/states/daily.json`
+        const stateHistory = await this.loadData(url);
+        return this.setState({
+            stateHistory
         })
     }
     
@@ -64,74 +70,299 @@ class CovidData extends Component {
             update: true,
             data: this.getStateData()
         }, this.handleChange(event));
-    }
+    } 
 
-
-
-    async componentDidMount() {
+    getTheStates = async () => {
         const url = `https://covidtracking.com/api/v1/states/current.json`
         const stateData = await this.loadData(url);
         this.setState({
-            stateData: stateData
-        })
-        this.getCountryData();
-        this.getChartData();
+        stateData: stateData
+        })   
     }
 
     handleChange = async (event) => {        
         this.setState({
             theState: event.target.value,
         }, this.getStateData);
+    }    
+
+    async componentDidMount() {
+        this.getTheStates();
+        this.getCountryData();
+        this.getChartData();
+        this.getStateHistory();
+        this.getStateData();
     }
 
- 
-
     render() {
+        const stateAbb = [
+            {
+                "name": "Alabama",
+                "abbreviation": "AL"
+            },
+            {
+                "name": "Alaska",
+                "abbreviation": "AK"
+            },
+            {
+                "name": "American Samoa",
+                "abbreviation": "AS"
+            },
+            {
+                "name": "Arizona",
+                "abbreviation": "AZ"
+            },
+            {
+                "name": "Arkansas",
+                "abbreviation": "AR"
+            },
+            {
+                "name": "California",
+                "abbreviation": "CA"
+            },
+            {
+                "name": "Colorado",
+                "abbreviation": "CO"
+            },
+            {
+                "name": "Connecticut",
+                "abbreviation": "CT"
+            },
+            {
+                "name": "Delaware",
+                "abbreviation": "DE"
+            },
+            {
+                "name": "District Of Columbia",
+                "abbreviation": "DC"
+            },
+            {
+                "name": "Federated States Of Micronesia",
+                "abbreviation": "FM"
+            },
+            {
+                "name": "Florida",
+                "abbreviation": "FL"
+            },
+            {
+                "name": "Georgia",
+                "abbreviation": "GA"
+            },
+            {
+                "name": "Guam",
+                "abbreviation": "GU"
+            },
+            {
+                "name": "Hawaii",
+                "abbreviation": "HI"
+            },
+            {
+                "name": "Idaho",
+                "abbreviation": "ID"
+            },
+            {
+                "name": "Illinois",
+                "abbreviation": "IL"
+            },
+            {
+                "name": "Indiana",
+                "abbreviation": "IN"
+            },
+            {
+                "name": "Iowa",
+                "abbreviation": "IA"
+            },
+            {
+                "name": "Kansas",
+                "abbreviation": "KS"
+            },
+            {
+                "name": "Kentucky",
+                "abbreviation": "KY"
+            },
+            {
+                "name": "Louisiana",
+                "abbreviation": "LA"
+            },
+            {
+                "name": "Maine",
+                "abbreviation": "ME"
+            },
+            {
+                "name": "Marshall Islands",
+                "abbreviation": "MH"
+            },
+            {
+                "name": "Maryland",
+                "abbreviation": "MD"
+            },
+            {
+                "name": "Massachusetts",
+                "abbreviation": "MA"
+            },
+            {
+                "name": "Michigan",
+                "abbreviation": "MI"
+            },
+            {
+                "name": "Minnesota",
+                "abbreviation": "MN"
+            },
+            {
+                "name": "Mississippi",
+                "abbreviation": "MS"
+            },
+            {
+                "name": "Missouri",
+                "abbreviation": "MO"
+            },
+            {
+                "name": "Montana",
+                "abbreviation": "MT"
+            },
+            {
+                "name": "Nebraska",
+                "abbreviation": "NE"
+            },
+            {
+                "name": "Nevada",
+                "abbreviation": "NV"
+            },
+            {
+                "name": "New Hampshire",
+                "abbreviation": "NH"
+            },
+            {
+                "name": "New Jersey",
+                "abbreviation": "NJ"
+            },
+            {
+                "name": "New Mexico",
+                "abbreviation": "NM"
+            },
+            {
+                "name": "New York",
+                "abbreviation": "NY"
+            },
+            {
+                "name": "North Carolina",
+                "abbreviation": "NC"
+            },
+            {
+                "name": "North Dakota",
+                "abbreviation": "ND"
+            },
+            {
+                "name": "Northern Mariana Islands",
+                "abbreviation": "MP"
+            },
+            {
+                "name": "Ohio",
+                "abbreviation": "OH"
+            },
+            {
+                "name": "Oklahoma",
+                "abbreviation": "OK"
+            },
+            {
+                "name": "Oregon",
+                "abbreviation": "OR"
+            },
+            {
+                "name": "Palau",
+                "abbreviation": "PW"
+            },
+            {
+                "name": "Pennsylvania",
+                "abbreviation": "PA"
+            },
+            {
+                "name": "Puerto Rico",
+                "abbreviation": "PR"
+            },
+            {
+                "name": "Rhode Island",
+                "abbreviation": "RI"
+            },
+            {
+                "name": "South Carolina",
+                "abbreviation": "SC"
+            },
+            {
+                "name": "South Dakota",
+                "abbreviation": "SD"
+            },
+            {
+                "name": "Tennessee",
+                "abbreviation": "TN"
+            },
+            {
+                "name": "Texas",
+                "abbreviation": "TX"
+            },
+            {
+                "name": "Utah",
+                "abbreviation": "UT"
+            },
+            {
+                "name": "Vermont",
+                "abbreviation": "VT"
+            },
+            {
+                "name": "Virgin Islands",
+                "abbreviation": "VI"
+            },
+            {
+                "name": "Virginia",
+                "abbreviation": "VA"
+            },
+            {
+                "name": "Washington",
+                "abbreviation": "WA"
+            },
+            {
+                "name": "West Virginia",
+                "abbreviation": "WV"
+            },
+            {
+                "name": "Wisconsin",
+                "abbreviation": "WI"
+            },
+            {
+                "name": "Wyoming",
+                "abbreviation": "WY"
+            }
+        ]
         const stateData = this.state.stateData;
-        console.log(stateData)
+        console.log(this.state)
         const { data } = this.state;
+        console.log(this.state.data)
         const theState = this.state.theState;
+        const upperTheState = theState.toUpperCase();
         const lcState = theState.toLowerCase();
         const countryData = this.state.countryData;
-        const chartData = this.state.chartData;
-        console.log("cd render", countryData)
-        function day1() {
-            if (chartData !== undefined) {
-                console.log("chart data at render => ", chartData[0].positive); return chartData[0].positive;
+        const matchingState = [];
+        stateAbb.map(element => {
+            if (upperTheState === element.abbreviation) {
+                console.log(element.name)
+                matchingState.push(element.name);
+            }}
+        )
+        console.log('matched state =>', matchingState);
+        const stateDateArray = [];    
+        const statePosArray = [];
+            if (this.state.stateHistory !== undefined) {
+            for (let i = 0; i < 768; i++) {
+                if (lcState === this.state.stateHistory[i].state.toLowerCase()) {
+                    const stateDate = this.state.stateHistory[i].date;
+                    const statePos = this.state.stateHistory[i].positive;
+                    stateDateArray.push(stateDate);
+                    statePosArray.push(statePos)
                 }
             }
-        function day2() {
-            if (chartData !== undefined) {
-                console.log("chart data at render2 => ", chartData[1].positive); return chartData[1].positive;
-                }
-            }
-        function day3() {
-            if (chartData !== undefined) {
-                console.log("chart data at render3 => ", chartData[2].positive); return chartData[2].positive;
-                }
-            }
-        function day4() {
-            if (chartData !== undefined) {
-                console.log("chart data at render3 => ", chartData[3].positive); return chartData[3].positive;
-                }
-            }
-        function day5() {
-            if (chartData !== undefined) {
-                console.log("chart data at render3 => ", chartData[4].positive); return chartData[4].positive;
-                }
-            }
-        function day6() {
-            if (chartData !== undefined) {
-                console.log("chart data at render3 => ", chartData[5].positive); return chartData[5].positive;
-                }
-            }
-        function day7() {
-            if (chartData !== undefined) {
-                console.log("chart data at render3 => ", chartData[6].positive); return chartData[6].positive;
-                }
-            }
-        
-        const newData = [{name: "7 Days", uv: day7(), pv: 2400, amt: 2400}, {name: '6 Days', uv: day6(), pv: 2400, amt: 2400}, {name: '5 Days', uv: day5(), pv: 2400, amt: 2400}, {name: '4 Days', uv: day4(), pv: 2400, amt: 2400}, {name: '3 Days', uv: day3(), pv: 2400, amt: 2400}, {name: 'Yesterday', uv: day2(), pv: 2400, amt: 2400}, {name: 'Today', uv: day1(), pv: 2400, amt: 2400}];
+        }
+
+        const newData = [{name: stateDateArray[13], uv: statePosArray[13], pv: 2400, amt: 2400}, {name: stateDateArray[12], uv: statePosArray[12], pv: 2400, amt: 2400}, {name: stateDateArray[11], uv: statePosArray[11], pv: 2400, amt: 2400}, {name: stateDateArray[10], uv: statePosArray[10], pv: 2400, amt: 2400},{name: stateDateArray[9], uv: statePosArray[9], pv: 2400, amt: 2400}, {name: stateDateArray[8], uv: statePosArray[8], pv: 2400, amt: 2400}, {name: stateDateArray[7], uv: statePosArray[7], pv: 2400, amt: 2400}, {name: stateDateArray[6], uv: statePosArray[6], pv: 2400, amt: 2400}, {name: stateDateArray[5], uv: statePosArray[5], pv: 2400, amt: 2400}, {name: stateDateArray[4], uv: statePosArray[4], pv: 2400, amt: 2400}, {name: stateDateArray[3], uv: statePosArray[3], pv: 2400, amt: 2400}, {name: stateDateArray[2], uv: statePosArray[2], pv: 2400, amt: 2400}, {name: stateDateArray[1], uv: statePosArray[1], pv: 2400, amt: 2400}, {name: stateDateArray[0], uv: statePosArray[0], pv: 2400, amt: 2400}];
         return (
             <div className="data">
                 <div className='resultsDiv'>
@@ -139,32 +370,32 @@ class CovidData extends Component {
                     <div className='usa'>
                         <h3 className='results1'>Positive Test: </h3><h2>{countryData.positive}</h2>
                         <h3 className='results1'>Total Deaths: </h3><h2>{countryData.death}</h2>
-                        <h3 className='results1'>Total Cases: </h3><h2>{countryData.total}  </h2>                     
+                        <h3 className='results1'>Total Test: </h3><h2>{countryData.total}  </h2>                     
                     </div>
                 </div>
                 <div className='resultsDiv2'>
-                <h2>Select a State</h2>
+                {/* <h2>Select a State</h2> */}
+                <h1>{matchingState[0]}</h1>
                 <form  value={this.state.theState} onChange={this.handleChange}>
                     <label>
                     <select className="ui selection dropdown">
                         {stateData.map((state) => <option  className="dropdown icon" key={state.state} value={state.state}>{state.state}</option>)}
                     </select> 
                     </label> 
-                </form  >
-
+                </form>
                     <img src={`http://flags.ox3.in/svg/us/${lcState}.svg`} className='flag2' alt='state flag'/>
                     <div>
                         <h3 className='results1'>Positive Test: </h3><h2>{data.positive}</h2>
                         <h3 className='results1'>Total Deaths: </h3><h2>{data.death}</h2>
-                        <h3 className='results1'>Total Cases: </h3><h2>{data.total}  </h2>                     
+                        <h3 className='results1'>Total Test: </h3><h2>{data.total}  </h2>                     
                     </div>
                 </div>
                 <hr />
                 <div className='lineChart'>
-                    <h1>7 Day Historical Chart</h1>
+                    <h1>14 Day Historical Chart (+ Test)</h1>
                     <LineChart width={500} height={200} data={newData}>
                         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                        <CartesianGrid stroke="#ccc" />
+                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
                         <XAxis dataKey="name" />
                         <YAxis />
                     </LineChart>
