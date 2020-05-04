ffbf1ebf2a11e7c95a8d5e3c822f151c60a319f4
Maalik McBurrows <mmcburrows1@gmail.com>
2020-05-04 14:10:19 -0400

    bringing over section titles

--- src/components/VerticalMenu.js
+++ src/components/VerticalMenu.js
@@ -9,15 +9,23 @@ function VerticalMenu() {
 
     return (
         <div id="vertmenu">
-            <div className='twittervm'>
+            {/* <div><h1>CDC Video Updates</h1></div> */}
+            <div className='cdcyttext'>
+                    CDC Video Updates
+                </div>
+            <div className='youtubevm'>
+                
+                <div>
+                    <Youtube />
+                </div>
+            </div> <hr />
+            <div className='cdcyttext'><h1>CDC Twitter Updates</h1></div>
+            <div className='youtubevm' id="twitterFeed">
                 <Twitter />
             </div>
-            <div className='youtubevm'>
-                {/* <Youtube /> */}
-                <CountryTable />
-            </div> <hr />
         </div>
     )
 }
 
 export default VerticalMenu;
+
